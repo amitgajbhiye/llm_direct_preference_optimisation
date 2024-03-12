dc20e17426ecca510aecfb97b2c28b8c683900cf
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-03-12 11:53:08 +0000

    4bit llama2-7B zero-shot prompting cs prompt

--- src/llm_prompting.py
+++ src/llm_prompting.py
@@ -1,26 +1,82 @@
 import gc
 import torch
 import transformers
+
 from transformers import AutoTokenizer
 
-torch.cuda.empty_cache()
-gc.collect()
-gc.collect()
+
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    AutoTokenizer,
+    TrainingArguments,
+    pipeline,
+)
+from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training
+from trl import SFTTrainer
 
 
 # model = "meta-llama/Llama-2-13b-chat-hf"
-# model = "meta-llama/Llama-2-7b-chat-hf"
-model = "meta-llama/Llama-2-7b-hf"
+base_model = "meta-llama/Llama-2-7b-chat-hf"
+# model = "meta-llama/Llama-2-7b-hf"
 # model =  "meta-llama/Llama-2-13b-hf"
 
 inp_file = "data/ufet/clean_types.txt"
 
+# Model Prepration
 
-tokenizer = AutoTokenizer.from_pretrained(model)
+# Quantization configuration
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_use_double_quant=True,
+)
+
+# LoRA configuration
+# peft_config = LoraConfig(
+#     r=16,
+#     lora_alpha=32,
+#     lora_dropout=0.05,
+#     bias="none",
+#     task_type="CAUSAL_LM",
+#     target_modules=[
+#         "up_proj",
+#         "down_proj",
+#         "gate_proj",
+#         "k_proj",
+#         "q_proj",
+#         "v_proj",
+#         "o_proj",
+#     ],
+# )
+
+
+# Load base moodel
+model = AutoModelForCausalLM.from_pretrained(
+    base_model, quantization_config=bnb_config, device_map={"": 0}
+)
+
+# Cast the layernorm in fp32, make output embedding layer require grads, add the upcasting of the lmhead to fp32
+# model = prepare_model_for_kbit_training(model)
+
+
+print(f"############ Model ############", end="\n\n")
+print(model)
+
+
+# Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(base_model, use_fast=True)
+tokenizer.pad_token = tokenizer.unk_token
+tokenizer.padding_side = "right"
+
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
-    torch_dtype=torch.float16,
     device_map="auto",
 )
 
@@ -64,7 +120,7 @@ with open(file_name, "w") as out_file:
             num_return_sequences=1,
             eos_token_id=tokenizer.eos_token_id,
             max_length=4000,
-            max_new_tokens=2000,
+            max_new_tokens=1000,
             return_full_text=False,
             repetition_penalty=1.0,
             length_penalty=1.0,
@@ -78,6 +134,12 @@ with open(file_name, "w") as out_file:
 
             print("===================================")
 
+del model
+del pipeline
+
+
+gc.collect()
+gc.collect()
 
 # parameters = {
 #     "max_length": 4000,
