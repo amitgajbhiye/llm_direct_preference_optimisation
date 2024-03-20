8c4189edefc88358c9b89d659a368c3f59fb44ea
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-03-20 11:25:02 +0000

    4bit dpo finetunning

--- src/dpo_training.py
+++ src/dpo_training.py
@@ -86,16 +86,16 @@ class ScriptArguments:
         default=1024, metadata={"help": "the maximum sequence length"}
     )
     max_steps: Optional[int] = field(
-        default=1000, metadata={"help": "max number of training steps"}
+        default=10, metadata={"help": "max number of training steps"}
     )
     logging_steps: Optional[int] = field(
-        default=10, metadata={"help": "the logging frequency"}
+        default=1, metadata={"help": "the logging frequency"}
     )
     save_steps: Optional[int] = field(
         default=100, metadata={"help": "the saving frequency"}
     )
     eval_steps: Optional[int] = field(
-        default=100, metadata={"help": "the evaluation frequency"}
+        default=1, metadata={"help": "the evaluation frequency"}
     )
 
     output_dir: Optional[str] = field(
@@ -223,7 +223,7 @@ if __name__ == "__main__":
 
     model = AutoModelForCausalLM.from_pretrained(
         script_args.model_name_or_path,
-        # low_cpu_mem_usage=True,
+        low_cpu_mem_usage=True,
         torch_dtype=torch_dtype,
         # load_in_4bit=script_args.load_in_4bit,
         device_map="auto",
