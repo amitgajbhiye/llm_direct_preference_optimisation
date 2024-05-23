8b9fd94543a16e775db686e9b69cadf288ce6ae6
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-05-23 11:27:01 +0100

    property embeddig from llm2vec

--- src/llm2vec.py
+++ src/llm2vec.py
@@ -4,15 +4,13 @@ from peft import PeftModel
 
 from llm2vec import LLM2Vec
 
+MODEL_ID = "McGill-NLP/LLM2Vec-Llama-2-7b-chat-hf-mntp-supervised"
+
 # Loading base Mistral model, along with custom code that enables bidirectional connections in decoder-only LLMs. MNTP LoRA weights are merged into the base model.
-tokenizer = AutoTokenizer.from_pretrained(
-    "McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp"
-)
-config = AutoConfig.from_pretrained(
-    "McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp", trust_remote_code=True
-)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+config = AutoConfig.from_pretrained("MODEL_ID", trust_remote_code=True)
 model = AutoModel.from_pretrained(
-    "McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp",
+    MODEL_ID,
     trust_remote_code=True,
     config=config,
     torch_dtype=torch.bfloat16,
@@ -20,14 +18,12 @@ model = AutoModel.from_pretrained(
 )
 model = PeftModel.from_pretrained(
     model,
-    "McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp",
+    MODEL_ID,
 )
 model = model.merge_and_unload()  # This can take several minutes on cpu
 
 # Loading supervised model. This loads the trained LoRA weights on top of MNTP model. Hence the final weights are -- Base model + MNTP (LoRA) + supervised (LoRA).
-model = PeftModel.from_pretrained(
-    model, "McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp-supervised"
-)
+model = PeftModel.from_pretrained(model, MODEL_ID)
 
 # Wrapper for encoding and pooling operations
 l2v = LLM2Vec(model, tokenizer, pooling_mode="mean", max_length=512)
