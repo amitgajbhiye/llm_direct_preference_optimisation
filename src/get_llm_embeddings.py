8bb9209e2cc89c280d0ea688989f34481e3d0b63
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-05-23 11:38:57 +0100

    property embeddig from llm2vec

--- src/get_llm_embeddings.py
+++ src/get_llm_embeddings.py
@@ -30,30 +30,13 @@ model = PeftModel.from_pretrained(
 # Wrapper for encoding and pooling operations
 l2v = LLM2Vec(model, tokenizer, pooling_mode="mean", max_length=512)
 
-# Encoding queries using instructions
-instruction = (
-    "Given a web search query, retrieve relevant passages that answer the query:"
-)
-queries = [
-    [instruction, "how much protein should a female eat"],
-    [instruction, "summit define"],
-]
-q_reps = l2v.encode(queries)
-
 # Encoding documents. Instruction are not required for documents
 documents = [
     "As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
     "Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments.",
 ]
+
 d_reps = l2v.encode(documents)
 
-# Compute cosine similarity
-q_reps_norm = torch.nn.functional.normalize(q_reps, p=2, dim=1)
-d_reps_norm = torch.nn.functional.normalize(d_reps, p=2, dim=1)
-cos_sim = torch.mm(q_reps_norm, d_reps_norm.transpose(0, 1))
-
-print(cos_sim)
-"""
-tensor([[0.5417, 0.0780],
-        [0.0627, 0.5726]])
-"""
+print("d_reps")
+print(d_reps)
