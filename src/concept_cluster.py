189224fced98ce7d3c4ffe8c8293e74afbb5ba82
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-05-28 15:47:00 +0100

    hdbscan concept clusters llama3_8b

--- src/concept_cluster.py
+++ src/concept_cluster.py
@@ -101,7 +101,9 @@ if cluster_algo == "DBSCAN":
 
 if cluster_algo == "HDBSCAN":
 
-    normalized_embeddings = normalize(llm_con_embeds, norm='l2')
+    # normalized_embeddings = normalize(llm_con_embeds, norm='l2')
+
+    normalized_embeddings = llm_con_embeds
 
     # cosine_distances = pdist(normalized_embeddings, metric='cosine')
     # cosine_distance_matrix = squareform(cosine_distances)
@@ -122,13 +124,35 @@ if cluster_algo == "HDBSCAN":
     # Experiment with different parameter values
     results = []
 
-    for min_cluster_size in range(2, 20):
-        for min_samples in range(2, 20):
-            cluster_labels, silhouette_avg = evaluate_hdbscan(min_cluster_size, min_samples)
-            results.append((min_cluster_size, min_samples, silhouette_avg, cluster_labels))
+    # for min_cluster_size in range(2, 20):
+    #     for min_samples in range(2, 20):
+    #         cluster_labels, silhouette_avg = evaluate_hdbscan(min_cluster_size, min_samples)
+    #         results.append((min_cluster_size, min_samples, silhouette_avg, cluster_labels))
+
+
+    cluster_labels, silhouette_avg = evaluate_hdbscan(min_cluster_size=3, min_samples=2)
 
     # Choose the best parameters based on silhouette score
     best_result = max(results, key=lambda x: x[2])
     best_min_cluster_size, best_min_samples, best_silhouette, best_cluster_labels = best_result
 
     print(f"Best min_cluster_size: {best_min_cluster_size}, Best min_samples: {best_min_samples}, Best silhouette_score: {best_silhouette}")
+
+    concept_cluster_list = [
+    (con, clus_label) for con, clus_label in zip(concepts, best_cluster_labels)
+]
+
+    con_cluster_df = pd.DataFrame.from_records(
+        concept_cluster_list, columns=["concept", "cluster_label"]
+    )
+
+    # property_cluster_df = pd.DataFrame.from_dict(property_cluster_map)
+    con_cluster_df.sort_values(by="cluster_label", inplace=True, ascending=False)
+
+    print(con_cluster_df)
+
+    con_cluster_df.to_csv(
+        f"data/ontology_concepts/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp/hbdscan_transport_con_cluster_llama38b_embeds.txt",
+        sep="\t",
+        index=None,
+    )
