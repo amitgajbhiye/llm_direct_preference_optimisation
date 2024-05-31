1864553db0b7ce2415b0bd673da3003ad4b31792
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-05-31 10:57:34 +0100

    concept properties based on category

--- src/llama3_based_on_categories.py
+++ src/llama3_based_on_categories.py
@@ -115,10 +115,10 @@ with open(file_name, "w") as out_file:
         for seq in sequences:
             # response_list.append(f"{seq['generated_text']}\n\n")
 
-            print(f"f'Category: {cat}, {seq["generated_text"]}'\n")
+            print(f'\nCategory: {cat}, {seq["generated_text"]}\n')
 
             # out_file.write(f"Category: {cat}\n")
-            out_file.write(f'Category: {cat}, {seq["generated_text"]}')
+            out_file.write(f'\nCategory: {cat}, {seq["generated_text"]}')
 
             print("===================================")
 
