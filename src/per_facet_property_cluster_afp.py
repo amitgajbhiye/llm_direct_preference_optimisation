93ebf4d9f58bab1ba011f019d03d9317c147f45d
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-06-06 10:32:00 +0100

    hdbscan clustering all properties

--- src/per_facet_property_cluster_afp.py
+++ src/per_facet_property_cluster_afp.py
@@ -58,29 +58,17 @@ uniq_properties = df["property"].unique()
 # print()
 # print()
 
-for i, facet in enumerate(uniq_facets):
-
-    facet_count = facet_count_dict[facet]
-
-    print(
-        f"****** Processing facet_no: {i}, facet: {facet}, facet_count: {facet_count} ******"
-    )
-
-    if facet_count < 2:
-        print(f"facet_count: {facet_count}, less than 2; ignoring facet")
-        continue
-
-    # print(f"facet_count: {facet_count}")
-    properties = df[df["facet"] == facet]["property"].unique()
+cluster_all_properties = True
+if cluster_all_properties:
 
     # print(f"num_property_for_facet: {len(properties)}")
 
-    llm_prop_embeds = l2v.encode(properties).detach().cpu().numpy()
+    llm_prop_embeds = l2v.encode(uniq_properties).detach().cpu().numpy()
 
     # print(f"llm_prop_embeds.shape: {llm_prop_embeds.shape}")
 
     prop_and_embedding = [
-        (prop, embed) for prop, embed in zip(properties, llm_prop_embeds)
+        (prop, embed) for prop, embed in zip(uniq_properties, llm_prop_embeds)
     ]
 
     scaler = StandardScaler()
@@ -112,7 +100,7 @@ for i, facet in enumerate(uniq_facets):
         labels = hdbscan_clusterer.fit_predict(embeddings_scaled)
 
     property_cluster_list = [
-        (prop, clus_label) for prop, clus_label in zip(properties, labels)
+        (prop, clus_label) for prop, clus_label in zip(uniq_properties, labels)
     ]
 
     sorted_property_cluster_list = sorted(property_cluster_list, key=lambda x: x[1])
@@ -121,58 +109,73 @@ for i, facet in enumerate(uniq_facets):
     for prop, cluster in sorted_property_cluster_list:
         print(prop, "\t", cluster)
 
-    # print("sorted_property_cluster_list")
-    # print(sorted_property_cluster_list)
+######
+# for i, facet in enumerate(uniq_facets):
 
-    print(f"****** Finished processing facet: {facet} ******")
-    print()
-    print()
+#     facet_count = facet_count_dict[facet]
 
-    ###########################################
-    # print("property_cluster_list")
-    # print(property_cluster_list)
+#     print(
+#         f"****** Processing facet_no: {i}, facet: {facet}, facet_count: {facet_count} ******"
+#     )
 
-    # property_cluster_map = {
-    #     prop: cluster_label for prop, cluster_label in zip(properties, labels)
-    # }
+#     if facet_count < 2:
+#         print(f"facet_count: {facet_count}, less than 2; ignoring facet")
+#         continue
 
-    # print("property_cluster_map")
-    # print(property_cluster_map)
+#     # print(f"facet_count: {facet_count}")
+#     properties = df[df["facet"] == facet]["property"].unique()
 
-    # clustered_properties = pd.DataFrame.from_records(
-    #     property_cluster_list, columns=["property", "cluster_label"]
-    # )
+#     # print(f"num_property_for_facet: {len(properties)}")
 
-    # clustered_properties.sort_values(by=["cluster_label"], inplace=True)
+#     llm_prop_embeds = l2v.encode(properties).detach().cpu().numpy()
 
-    # print(f"clustered_properties")
-    # print(clustered_properties)
+#     # print(f"llm_prop_embeds.shape: {llm_prop_embeds.shape}")
 
-    # clustered_properties.to_csv(
-    #     "clustered_properties.txt",
-    #     sep="\t",
-    #     index=False,
-    # )
+#     prop_and_embedding = [
+#         (prop, embed) for prop, embed in zip(properties, llm_prop_embeds)
+#     ]
 
-    # def assign_prop_cluster_label(prop):
-    #     return property_cluster_map[prop]
+#     scaler = StandardScaler()
+#     embeddings_scaled = scaler.fit_transform(llm_prop_embeds)
 
-    # # for k, v in property_cluster_map.items():
-    # #     print(k, v)
+#     clustering_algorithm = "hdbscan"
 
-    # cluster_df = df.copy(deep=True)
+#     if clustering_algorithm == "affinity_propagation":
+#         affinity_propagation = AffinityPropagation()
+#         # Fit the model to your embeddings
+#         affinity_propagation.fit(embeddings_scaled)
 
-    # cluster_df["cluster_label"] = cluster_df["property"].apply(
-    #     assign_prop_cluster_label
-    # )
+#         # Get the cluster centers and labels
+#         cluster_centers_indices = affinity_propagation.cluster_centers_indices_
+#         labels = affinity_propagation.labels_
 
-    # cluster_df.sort_values(by=["cluster_label"], inplace=True)
+#     elif clustering_algorithm == "dbscan":
 
-    # cluster_df.to_csv(
-    #     "concept_property_label.txt",
-    #     sep="\t",
-    #     index=False,
-    # )
+#         # dbscan_clusterer = DBSCAN(
+#         #     eps=0.5, min_samples=3, metric="cosine", algorithm="brute"
+#         # )
+#         dbscan_clusterer = DBSCAN(eps=0.5, min_samples=5)
+#         labels = dbscan_clusterer.fit_predict(embeddings_scaled)
 
-    # print("cluster_df")
-    # print(cluster_df)
+#     elif clustering_algorithm == "hdbscan":
+
+#         # hdbscan_clusterer = hdbscan.HDBSCAN(min_cluster_size=10, min_samples=5)
+#         hdbscan_clusterer = hdbscan.HDBSCAN()
+#         labels = hdbscan_clusterer.fit_predict(embeddings_scaled)
+
+#     property_cluster_list = [
+#         (prop, clus_label) for prop, clus_label in zip(properties, labels)
+#     ]
+
+#     sorted_property_cluster_list = sorted(property_cluster_list, key=lambda x: x[1])
+
+#     print("property", "\t", "cluster_label")
+#     for prop, cluster in sorted_property_cluster_list:
+#         print(prop, "\t", cluster)
+
+#     # print("sorted_property_cluster_list")
+#     # print(sorted_property_cluster_list)
+
+#     print(f"****** Finished processing facet: {facet} ******")
+#     print()
+#     print()
