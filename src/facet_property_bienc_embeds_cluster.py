ea721c23a3ff98d5ebde0a4630a9411d739e3146
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-06-12 10:39:37 +0100

    hdbscan biencoder embedding clustering

--- src/facet_property_bienc_embeds_cluster.py
+++ src/facet_property_bienc_embeds_cluster.py
@@ -3,7 +3,7 @@ import pickle
 import hdbscan
 import numpy as np
 import pandas as pd
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN, AffinityPropagation
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 
@@ -21,110 +21,156 @@ prop_embeddings = np.array(list(prop_embed.values()))
 print(f"Properties: {properties[0:10]} ...")
 print(f"prop_embeddings: {prop_embeddings.shape} ...")
 
-cluster_algo = "HDBSCAN"
+
+cluster_algo = "affinity_propogation"
 print(f"cluster_algo:", cluster_algo)
 
-# if cluster_algo == "DBSCAN":
+if cluster_algo == "affinity_propogation":
 
-#     def dbscan_clustering(X, eps_values, min_samples_values):
+    X = StandardScaler().fit_transform(prop_embeddings)
 
-#         best_score = -1
-#         best_params = None
-#         best_labels = None
+    def affinity_propagation_clustering(X, damping_values, preference_values):
 
-#         for eps in eps_values:
-#             for min_samples in min_samples_values:
-#                 db = DBSCAN(
-#                     eps=eps, min_samples=min_samples, metric="cosine", algorithm="brute"
-#                 ).fit(X)
-#                 labels = db.labels_
+        best_score = -1
+        best_params = None
+        best_labels = None
 
-#                 # Ignore clusters where all points are classified as noise
-#                 if len(set(labels)) <= 1:
-#                     continue
+        for damping in damping_values:
+            for preference in preference_values:
+                clustering = AffinityPropagation(damping=damping, preference=None).fit(
+                    X
+                )
+                labels = clustering.labels_
 
-#                 # Calculate silhouette score
-#                 score = silhouette_score(X, labels)
+                # Ignore clusters where all points are classified as noise
+                if len(set(labels)) <= 1:
+                    continue
 
-#                 # Check if we got a better score
-#                 if score > best_score:
-#                     best_score = score
-#                     best_params = (eps, min_samples)
-#                     best_labels = labels
+                # Calculate silhouette score
+                score = silhouette_score(X, labels)
 
-#         return best_score, best_params, best_labels
+                # Check if we got a better score
+                if score > best_score:
+                    best_score = score
+                    best_params = (damping, preference)
+                    best_labels = labels
 
-#     # eps_values = np.arange(0.1, 1.1, 0.1)
-#     # min_samples_values = range(2, 10)
+        print(f"len: {len(set(list(best_labels)))}")
+        return best_score, best_params, best_labels
 
-#     eps_values = [0.5]
-#     min_samples_values = [5]
+    # damping_values = np.arange(0.5, 1.0, 0.05)
+    # preference_values = np.arange(-50, 0, 5)
 
-#     X = StandardScaler().fit_transform(prop_embeddings)
+    damping_values = [0.5]
+    preference_values = [5]
 
-#     best_score, best_params, best_labels = dbscan_clustering(
-#         X, eps_values, min_samples_values
-#     )
+    best_score, best_params, best_labels = affinity_propagation_clustering(
+        X, damping_values, preference_values
+    )
 
-#     print(f"Best Silhouette Score: {best_score}")
-#     print(f"Best Parameters: eps={best_params[0]}, min_samples={best_params[1]}")
-
-#     # Best Silhouette Score: 0.02702467143535614
-#     # Best Parameters: eps=0.5, min_samples=5
-
-#     with open("bienc_cluster_colon_sep.txt", "w") as out_file:
-#         for prop, label in zip(properties, best_labels):
-#             out_file.write(f"{prop}\t{label}\n")
+    print(f"Best Silhouette Score: {best_score}")
+    print(f"Best Parameters: damping={best_params[0]}, preference={best_params[1]}")
 
 
-# if cluster_algo == "HDBSCAN":
+if cluster_algo == "DBSCAN":
+
+    def dbscan_clustering(X, eps_values, min_samples_values):
+
+        best_score = -1
+        best_params = None
+        best_labels = None
+
+        for eps in eps_values:
+            for min_samples in min_samples_values:
+                db = DBSCAN(
+                    eps=eps, min_samples=min_samples, metric="cosine", algorithm="brute"
+                ).fit(X)
+                labels = db.labels_
+
+                # Ignore clusters where all points are classified as noise
+                if len(set(labels)) <= 1:
+                    continue
+
+                # Calculate silhouette score
+                score = silhouette_score(X, labels)
+
+                # Check if we got a better score
+                if score > best_score:
+                    best_score = score
+                    best_params = (eps, min_samples)
+                    best_labels = labels
+
+        return best_score, best_params, best_labels
+
+    # eps_values = np.arange(0.1, 1.1, 0.1)
+    # min_samples_values = range(2, 10)
+
+    eps_values = [0.5]
+    min_samples_values = [5]
+
+    X = StandardScaler().fit_transform(prop_embeddings)
+
+    best_score, best_params, best_labels = dbscan_clustering(
+        X, eps_values, min_samples_values
+    )
+
+    print(f"Best Silhouette Score: {best_score}")
+    print(f"Best Parameters: eps={best_params[0]}, min_samples={best_params[1]}")
+
+    # Best Silhouette Score: 0.02702467143535614
+    # Best Parameters: eps=0.5, min_samples=5
+
+    with open("bienc_cluster_colon_sep.txt", "w") as out_file:
+        for prop, label in zip(properties, best_labels):
+            out_file.write(f"{prop}\t{label}\n")
 
 
-def hdbscan_clustering(X, min_cluster_size_values, min_samples_values):
-    print(f"In hdbscan_clustering")
-    best_score = -1
-    best_params = None
-    best_labels = None
+if cluster_algo == "HDBSCAN":
 
-    for min_cluster_size in min_cluster_size_values:
-        for min_samples in min_samples_values:
-            clusterer = hdbscan.HDBSCAN(
-                min_cluster_size=min_cluster_size, min_samples=min_samples
-            )
-            labels = clusterer.fit_predict(X)
+    def hdbscan_clustering(X, min_cluster_size_values, min_samples_values):
+        print(f"In hdbscan_clustering")
+        best_score = -1
+        best_params = None
+        best_labels = None
 
-            # Ignore clusters where all points are classified as noise
-            if len(set(labels)) <= 1 or (len(set(labels)) == 2 and -1 in labels):
-                continue
+        for min_cluster_size in min_cluster_size_values:
+            for min_samples in min_samples_values:
+                clusterer = hdbscan.HDBSCAN(
+                    min_cluster_size=min_cluster_size, min_samples=min_samples
+                )
+                labels = clusterer.fit_predict(X)
 
-            # Calculate silhouette score
-            score = silhouette_score(X, labels)
+                # Ignore clusters where all points are classified as noise
+                if len(set(labels)) <= 1 or (len(set(labels)) == 2 and -1 in labels):
+                    continue
 
-            # Check if we got a better score
-            if score > best_score:
-                best_score = score
-                best_params = (min_cluster_size, min_samples)
-                best_labels = labels
+                # Calculate silhouette score
+                score = silhouette_score(X, labels)
 
-            print(f"min_cluster_size: {min_cluster_size}")
-            print(f"min_samples: {min_samples}")
-            print(f"score: {score}")
-            print(f"best_score: {best_score}")
-            print()
+                # Check if we got a better score
+                if score > best_score:
+                    best_score = score
+                    best_params = (min_cluster_size, min_samples)
+                    best_labels = labels
 
-    return best_score, best_params, best_labels
+                print(f"min_cluster_size: {min_cluster_size}")
+                print(f"min_samples: {min_samples}")
+                print(f"score: {score}")
+                print(f"best_score: {best_score}")
+                print()
 
+        return best_score, best_params, best_labels
 
-min_cluster_size_values = range(3, 10)
-min_samples_values = range(1, 10)
+    min_cluster_size_values = range(3, 10)
+    min_samples_values = range(1, 10)
 
-X = StandardScaler().fit_transform(prop_embeddings)
+    X = StandardScaler().fit_transform(prop_embeddings)
 
-best_score, best_params, best_labels = hdbscan_clustering(
-    X, min_cluster_size_values, min_samples_values
-)
+    best_score, best_params, best_labels = hdbscan_clustering(
+        X, min_cluster_size_values, min_samples_values
+    )
 
-print(f"Best Silhouette Score: {best_score}")
-print(
-    f"Best Parameters: min_cluster_size={best_params[0]}, min_samples={best_params[1]}"
-)
+    print(f"Best Silhouette Score: {best_score}")
+    print(
+        f"Best Parameters: min_cluster_size={best_params[0]}, min_samples={best_params[1]}"
+    )
