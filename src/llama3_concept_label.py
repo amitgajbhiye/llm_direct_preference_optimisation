b7535f8a84087c2c204a9c6d4b3a2e0d0740eb27
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-06-14 13:56:13 +0100

    common label clustered concepts

---
+++ src/llama3_concept_label.py
@@ -0,0 +1,132 @@
+import gc
+import json
+
+import pandas as pd
+import torch
+import transformers
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    pipeline,
+)
+
+base_model = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+
+# Quantization configuration
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True,
+)
+
+# Load base moodel in quantised form
+model = AutoModelForCausalLM.from_pretrained(
+    base_model, quantization_config=bnb_config, device_map={"": 0}
+)
+
+print(f"############ Model ############", end="\n\n")
+print(model, end="\n\n")
+
+
+# Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(base_model, use_fast=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+pipeline = transformers.pipeline(
+    "text-generation", model=model, device_map="auto", tokenizer=tokenizer
+)
+
+concept_facet_property_file = "data/ontology_concepts/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp/facet_property/final_concept_facet_propert_clusterlabel.txt"
+
+df = pd.read_csv(concept_facet_property_file, sep="\t")
+
+concept_cluster_labels = df[["concept", "cluster_label"]]
+cluster_labels = concept_cluster_labels["cluster_label"].unique()
+
+print(f"input_df")
+print(df)
+
+
+llama3_8B_concepts_common_label_prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+You are an ontology engineer building a transport ontology.
+All output must include only valid JSON like the following example {"concepts": list of concepts, "class": [class of concepts in less than ten words]}.
+Don't add any explanations before and after the JSON.
+If you don't know the answer, please don't share false information.<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+
+You are an ontology engineer building a transport ontology. In the ontology what class would you assign to the following group of concepts <CONCEPT_LIST>.<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>"""
+
+
+file_name = "llama3_common_label_clustered_concepts.txt"
+print(f"Prompt used is : {llama3_8B_concepts_common_label_prompt}")
+
+concepts_common_label = []
+
+with open(file_name, "w") as out_file:
+
+    for cl_label in cluster_labels:
+        print(f"cluster_label:", cl_label)
+
+        concepts_list = concept_cluster_labels[
+            concept_cluster_labels["cluster_label"] == cl_label
+        ]["concept"].to_list()
+
+        num_clustered_concepts = len(concepts_list)
+
+        if num_clustered_concepts >= 10:
+            prompt_concepts = concepts_list[:10]
+        else:
+            prompt_concepts = concepts_list
+
+        prompt_concepts = ", ".join(prompt_concepts)
+
+        concept_prompt = llama3_8B_concepts_common_label_prompt.replace(
+            "<CONCEPT_LIST>", prompt_concepts
+        )
+
+        print(f"concepts_list:{concepts_list}")
+        print(f"prompt_concepts:{prompt_concepts}")
+
+        print(f"concept_prompt: {concept_prompt}")
+
+        sequences = pipeline(
+            concept_prompt,
+            do_sample=True,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+            max_new_tokens=500,
+            return_full_text=False,
+            repetition_penalty=1.0,
+            length_penalty=1.0,
+            truncation=True,
+            # max_length=500,
+            # top_p=,
+            # top_k=,
+        )
+
+        for seq in sequences:
+            # response_list.append(f"{seq['generated_text']}\n\n")
+            print(f"{seq['generated_text']}\n")
+
+            out_file.write(f"prompt_concepts:{prompt_concepts}")
+            out_file.write(f'{seq["generated_text"]}')
+
+            # concept_facet_generated_data.append((concept, facet, seq["generated_text"]))
+
+            print("===================================")
+
+        del seq
+        del sequences
+
+del model
+del pipeline
+
+gc.collect()
+gc.collect()
+gc.collect()
