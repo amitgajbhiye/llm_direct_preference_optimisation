1655d89d000399bc5448eea631733aad8eb60ffe
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-06-26 14:12:53 +0100

    code restructure

---
+++ src/llama3_concept_facet_property_prompting_new.py
@@ -0,0 +1,194 @@
+import gc
+import json
+import logging
+import os
+import time
+from argparse import ArgumentParser
+
+import pandas as pd
+import torch
+import transformers
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    pipeline,
+)
+
+start_time = time.time()
+
+
+def read_config(config_file):
+    with open(config_file, "r") as json_file:
+        config_dict = json.load(json_file)
+
+    return config_dict
+
+
+llama3_8B_1inc_prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+You are a contestant in the general knowledge quiz contest and always answer all kinds of common sense questions accurately. 
+All output must include only valid JSON like the following example {"concept": concept, "facet_properties_dict": {facet: [list of properties with each property less than ten words long]}}.
+Don't add any explanations before and after the JSON.
+If you don't know the answer, please don't share false information.<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+
+For the concept of the banana, write its different facets and most salient properties under each facet.<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+
+{"concept": "banana", "facet_properties_dict": {"category": ["fruit", "healthy snack", "food"], "color": ["yellow", "green"], "shape": ["curved"], "taste": ["sweet"], "nutritional content": ["rich in potassium", "high in sugar"], "used for": ["healthy snack", "making cakes", "making smoothie"], "located at": ["banana tree", "supermarket", "fridge", "fruit bowl"]}}<|eot_id|>
+
+For the concept of the <CONCEPT>, write its different facets and most salient properties under each facet.<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>"""
+
+
+def prepare_data(config):
+
+    input_file = config["input_file"]
+
+    # with open(economics_ontology_file, "r") as inp_file:
+    #     concepts = inp_file.readlines()
+    # concepts = [con.strip("\n").replace("_", " ").lower() for con in concepts]
+
+    df = pd.read_csv(input_file, sep="\t", names=["id", "concept"])
+    concepts = [con.strip() for con in df["concept"].values]
+
+    print(f"Number of concepts: {len(concepts)}")
+    print(f"input_concepts: {concepts}")
+
+    print(f"Prompt used is : {llama3_8B_1inc_prompt}")
+
+    concept_prompts = [
+        llama3_8B_1inc_prompt.replace("<CONCEPT>", con) for con in concepts
+    ]
+
+    return concept_prompts
+
+
+def generate_data(config):
+
+    base_model = config["base_model"]
+
+    # Quantization configuration
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True,
+    )
+
+    # Load base moodel in quantised form
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model, quantization_config=bnb_config, device_map=0
+    )
+
+    print(f"############ Model ############", end="\n\n")
+    print(model, end="\n\n")
+    print(f"Device map")
+    print(model.hf_device_map)
+
+    # Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(base_model, use_fast=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    pipeline = transformers.pipeline(
+        "text-generation", model=model, device_map="auto", tokenizer=tokenizer
+    )
+
+    repeat_times = config["repeat_times"]
+    output_file = config["output_file"]
+    batch_size = config["batch_size"]
+
+    total_batches = len(concept_prompts) // batch_size
+
+    with open(output_file, "w") as out_file:
+
+        for i in range(repeat_times):
+            print(f"****** repeat_times : {i} ******")
+
+            for batch_no, batch_start_idx in enumerate(
+                range(0, len(concept_prompts), batch_size)
+            ):
+                print(f"****** processing_batch: {batch_no} / {total_batches} ******")
+
+                concept_prompt_batch = concept_prompts[
+                    batch_start_idx : batch_start_idx + batch_size
+                ]
+
+                sequences = pipeline(
+                    concept_prompt_batch,
+                    do_sample=True,
+                    num_return_sequences=1,
+                    eos_token_id=tokenizer.eos_token_id,
+                    max_new_tokens=500,
+                    return_full_text=False,
+                    repetition_penalty=1.0,
+                    length_penalty=1.0,
+                    truncation=True,
+                    # max_length=500,
+                    # top_p=,
+                    # top_k=,
+                )
+
+                for seq in sequences:
+                    print(f"{seq[0]['generated_text']}\n", flush=True)
+
+                    logger.info(f"{seq[0]['generated_text']}\n")
+
+                    out_file.write(f'{seq[0]["generated_text"]}')
+
+                    print("===================================", flush=True)
+
+                del seq
+                del sequences
+
+    del model
+    del pipeline
+    del concept_prompts
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    hours = int(elapsed_time // 3600)
+    minutes = int((elapsed_time % 3600) // 60)
+    seconds = elapsed_time % 60
+
+    print(
+        f"Execution time: {hours} hours, {minutes} minutes, and {seconds:.2f} seconds"
+    )
+
+    gc.collect()
+    gc.collect()
+    gc.collect()
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description="Fine tune configuration")
+
+    parser.add_argument(
+        "--config_file",
+        default="configs/default_config.json",
+        help="path to the configuration file",
+    )
+    args = parser.parse_args()
+
+    config = read_config(args.config_file)
+
+    log_dir = config["log_dir"]
+    log_file_name = os.path.join(
+        log_dir,
+        f"log_{config['experiment_name']}_{time.strftime('%d-%m-%Y_%H-%M-%S')}.txt",
+    )
+    logging.basicConfig(
+        level=logging.INFO,
+        filename=log_file_name,
+        filemode="w",
+        format="%(asctime)s : %(name)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info(f"Reading Configuration File: {args.config_file}")
+
+    logger.info("The model is run with the following configuration")
+    logger.info(f"\n {config} \n")
