214ff3f62ab5378291872ec783e5504f48a84737
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-06-27 17:49:26 +0100

    llama3 science ev facet prop generation

--- src/llama3_concept_facet_property_prompting.py
+++ src/llama3_concept_facet_property_prompting.py
@@ -163,12 +163,12 @@ def generate_data(config, concept_prompts):
                 batch_end_time = time.time()
 
                 print(
-                    f"repeat_times: {i}, processed_batch: {batch_no} / {total_batches}, batch_size: {batch_size}, time_take: {get_execution_time(batch_start_time, batch_end_time)}",
+                    f"repeat_times: {i}, processed_batch: {batch_no} / {total_batches}, batch_size: {batch_size}, {get_execution_time(batch_start_time, batch_end_time)}",
                     flush=True,
                 )
 
                 logger.info(
-                    f"repeat_times: {i}, processed_batch: {batch_no} / {total_batches}, batch_size: {batch_size}, time_take: {get_execution_time(batch_start_time, batch_end_time)}"
+                    f"repeat_times: {i}, processed_batch: {batch_no} / {total_batches}, batch_size: {batch_size}, {get_execution_time(batch_start_time, batch_end_time)}"
                 )
 
     del model
