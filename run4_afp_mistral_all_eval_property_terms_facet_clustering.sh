bdaf52e9877088faff63a5d48b410d9afb3f5a6d
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-08-06 15:28:37 +0100

    affinity prop clustering

---
+++ run4_afp_mistral_all_eval_property_terms_facet_clustering.sh
@@ -0,0 +1,28 @@
+#!/bin/bash --login
+
+#SBATCH -A scw1858
+#SBATCH --job-name=commAFP
+
+#SBATCH --output=logs/run4_afp_mistral_all_eval_property_terms_facet_clustering.txt
+#SBATCH --error=logs/run4_afp_mistral_all_eval_property_terms_facet_clustering.txt
+
+#SBATCH --nodes=1
+#SBATCH -p highmem
+
+#SBATCH --ntasks=20
+#SBATCH --ntasks-per-node=20
+#SBATCH --cpus-per-task=1
+
+#SBATCH --mem=80G
+#SBATCH -t 0-12:00:00
+
+conda activate ven
+
+python3 src/facet_property_clustering.py --config_file configs/clustering/7_mistral7b_inst_mntp_property_terms_facet_embeds_science.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/7_mistral7b_inst_mntp_property_terms_facet_embeds_food.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/7_mistral7b_inst_mntp_property_terms_facet_embeds_equipment.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/7_mistral7b_inst_mntp_property_terms_facet_embeds_environment.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/7_mistral7b_inst_mntp_property_terms_facet_embeds_commonsense.json
+
+
+echo 'Job Finished !!!'
