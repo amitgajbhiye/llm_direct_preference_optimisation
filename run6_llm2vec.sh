2e490bc549f8b1df8841e3dd4c9518ddf2704177
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-08-07 14:15:47 +0100

    llm2vec embeds

---
+++ run6_llm2vec.sh
@@ -0,0 +1,23 @@
+#!/bin/bash --login
+
+#SBATCH -A scw1858
+#SBATCH --job-name=llm2vec
+
+#SBATCH --output=logs/out_llm2vec_embeds.txt
+#SBATCH --error=logs/err_llm2vec_embeds.txt
+
+#SBATCH --nodes=1
+#SBATCH -p highmem
+
+#SBATCH --ntasks=20
+#SBATCH --ntasks-per-node=20
+#SBATCH --cpus-per-task=1
+
+#SBATCH --mem=35G
+#SBATCH -t 0-03:00:00
+
+conda activate ven
+
+python3 src/embeds_llm2vec.py
+
+echo 'Job Finished !!!'
