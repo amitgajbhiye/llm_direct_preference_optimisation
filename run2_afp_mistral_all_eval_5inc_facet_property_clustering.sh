998892f30bf4a505fd20753d764079dac48e4cb9
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-08-12 15:14:59 +0100

    mistral affinity prop clustering

---
+++ run2_afp_mistral_all_eval_5inc_facet_property_clustering.sh
@@ -0,0 +1,27 @@
+#!/bin/bash --login
+
+#SBATCH -A scw1858
+#SBATCH --job-name=commAFP
+
+#SBATCH --output=logs/out_run2_afp_mistral_all_eval_5inc_facet_property_clustering.txt
+#SBATCH --error=logs/err_run2_afp_mistral_all_eval_5inc_facet_property_clustering.txt
+
+#SBATCH --nodes=1
+#SBATCH -p highmem
+
+#SBATCH --ntasks=7
+#SBATCH --ntasks-per-node=7
+#SBATCH --cpus-per-task=1
+
+#SBATCH --mem=75G
+#SBATCH -t 0-13:00:00
+
+conda activate venv
+
+python3 src/facet_property_clustering.py --config_file configs/clustering/10_mistral7b_inst_mntp_facet_colon_property_embeds_5inc_food.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/10_mistral7b_inst_mntp_facet_colon_property_embeds_5inc_equipment.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/10_mistral7b_inst_mntp_facet_colon_property_embeds_5inc_science.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/10_mistral7b_inst_mntp_facet_colon_property_embeds_5inc_environment.json
+python3 src/facet_property_clustering.py --config_file configs/clustering/10_mistral7b_inst_mntp_facet_colon_property_embeds_5inc_commonsense.json
+
+echo 'Job Finished !!!'
