61519993d1e05c071fe9adf7ddfee15db96090dc
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-08-29 10:59:26 +0100

    repeat1 1inc llama prompting

--- run_prompting.zsh
+++ run_prompting.zsh
@@ -1,12 +1,20 @@
 #!/bin/zsh
 
-LOG_FILE="logs/facet_prop_generation/run1_llama38b_all_eval_taxonomies_few_shot_prompting_zsh_script_log.txt"
+LOG_FILE="logs/facet_prop_generation/run1_llama38b_all_eval_taxonomies_1inc_prompting_zsh_script_log.txt"
+
+# scripts=(
+#     "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_food_facet_property.json"
+#     "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_equipment_facet_property.json"
+#     "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_science_facet_property.json"
+#     "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_environment_facet_property.json"
+# )
 
 scripts=(
-    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_food_facet_property.json"
-    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_equipment_facet_property.json"
-    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_science_facet_property.json"
-    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/2_llama3_5inc_environment_facet_property.json"
+    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/3_llama3_1inc_food_repeat_1.json"
+    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/3_llama3_1inc_equipment_repeat_1.json"
+    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/3_llama3_1inc_science_repeat_1.json"
+    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/3_llama3_1inc_environment_repeat_1.json"    
+    "python3 src/concept_facet_property_prompting.py --config configs/facet_prop_generation/3_llama3_1inc_commonsense_repeat_1.json"
 )
 
 for cmd in "${scripts[@]}"; do
