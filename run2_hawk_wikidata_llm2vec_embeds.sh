33deb60618a6b58dffe8dca34135e1ef53e9e71b
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2024-09-06 15:35:51 +0100

    wikidata llm2vec embedsings

---
+++ run2_hawk_wikidata_llm2vec_embeds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=wikiembeds
+#SBATCH --output=logs/wikidata/out_run2_hawk_wikidata_llm2vec_embeds.txt
+#SBATCH --error=logs/wikidata/err_run2_hawk_wikidata_llm2vec_embeds.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu_v100
+#SBATCH --gres=gpu:1
+#SBATCH --exclusive
+
+#SBATCH --mem=64G
+#SBATCH -t 0-6:00:00
+
+conda activate brand_new
+
+python src/embeds_llm2vec.py
+
+echo 'Job Finished !!!'
